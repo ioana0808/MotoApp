5ed3a94a6f0f7b53692f5a761139d7dea1ed151a
ioana <ioana.lacatus@student.upt.ro>
2020-05-11 20:35:30 +0300

    ui update not done but works

--- app/src/main/java/com/android/example/motoapp/DashboardActivity.kt
+++ app/src/main/java/com/android/example/motoapp/DashboardActivity.kt
@@ -1,19 +1,21 @@
 package com.android.example.motoapp
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity(){
 
-
+   private lateinit var mDrawerLayout:DrawerLayout
+    private lateinit var mToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-
 
         StartRouteBtn?.setOnClickListener {
 
@@ -26,12 +28,20 @@ class DashboardActivity : AppCompatActivity(){
             finish()
         }
 
-
-
-
+        mDrawerLayout=findViewById(R.id.drawerLayout)
+        mToggle= ActionBarDrawerToggle(this,mDrawerLayout,R.string.register,R.string.routes)
+        mDrawerLayout.addDrawerListener(mToggle)
+        mToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
 
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
